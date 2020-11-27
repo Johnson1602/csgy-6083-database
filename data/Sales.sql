95c77adf8e4039e26f035f1efc7ac940e588d9a1
Weiyi Xu <johnsonxwy@gmail.com>
2020-11-27 11:46:13 -0500

    update data

--- data/Sales.sql
+++ data/Sales.sql
@@ -1,16 +1,17 @@
---iphone 11
+-- iphone 11
 insert into sales (year,season,quantity) values (2019,'Q1',1000000);
 insert into sales (year,season,quantity) values (2019,'Q2',900000);
 insert into sales (year,season,quantity) values (2019,'Q3',800000);
 insert into sales (year,season,quantity) values (2019,'Q4',1000000);
---Alienware Aurora R11
+-- Alienware Aurora R11
 insert into sales (year,season,quantity) values (2020,'Q2',5000);
 insert into sales (year,season,quantity) values (2020,'Q3',8000);
---ipad pro 2018
-insert into sales (year,season,quantity) values (2018,'Q2',300000);
-insert into sales (year,season,quantity) values (2018,'Q3',420000);
-insert into sales (year,season,quantity) values (2018,'Q4',477000);
---Samsung s10
+-- ipad pro 2018
+insert into sales (year,season,quantity) values (2019,'Q1',350000);
+insert into sales (year,season,quantity) values (2019,'Q2',300000);
+insert into sales (year,season,quantity) values (2019,'Q3',420000);
+insert into sales (year,season,quantity) values (2019,'Q4',477000);
+-- Samsung S10
 insert into sales (year,season,quantity) values (2019,'Q1',200000);
 insert into sales (year,season,quantity) values (2019,'Q2',500000);
 insert into sales (year,season,quantity) values (2019,'Q3',400000);
